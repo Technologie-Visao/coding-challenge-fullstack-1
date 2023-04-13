72f01a799362425b241f25367b2c967d33a4259c
Bogdan <bogdan.pechounov@gmail.com>
2023-04-13 13:32:30 -0400

    setup tests

---
+++ backend/README.md
@@ -0,0 +1,16 @@
+# Instructions
+
+To start the server, use the following commands:
+
+```console
+npm install
+npm start
+```
+
+For testing and test coverage:
+
+```console
+npm test
+npm run test-watch
+npm run coverage
+```
---
+++ backend/tests/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest')
+const app = require('../src/app.js')
+
+describe('App', () => {
+  test('responds with a default message', async () => {
+    const response = await request(app).get('/')
+    expect(response.statusCode).toBe(200)
+    expect(response.text.length).toBeGreaterThan(0)
+  })
+})
